c0cb1d22b7ffc33ddaa221c841188cad277807dd
liujun286 <liujun26@jd.com>
2019-09-02 17:17:36 +0800

    keys to const

--- tools/src/main/java/com/neal/xposed/tools/pref/Keys.kt
+++ tools/src/main/java/com/neal/xposed/tools/pref/Keys.kt
@@ -1,8 +1,8 @@
 package com.neal.xposed.tools.pref
 
-val PREFERENCE_NAME_SETTINGS: String = "xposed_settings"
-val AUTO_COLLECT_INTERVAL_OPEN_KEY: String = "auto_collect_interval_open"
-val AUTO_COLLECT_OPEN_KEY: String = "auto_collect_open"
-val AUTO_COLLECT_INTERVAL_KEY: String = "auto_collect_interval"
-val AUTO_COLLECT_WHITE_LIST: String = "auto_collect_white_list"
-val AUTO_FEED: String = "auto_feed"
+const val PREFERENCE_NAME_SETTINGS: String = "xposed_settings"
+const val AUTO_COLLECT_INTERVAL_OPEN_KEY: String = "auto_collect_interval_open"
+const val AUTO_COLLECT_OPEN_KEY: String = "auto_collect_open"
+const val AUTO_COLLECT_INTERVAL_KEY: String = "auto_collect_interval"
+const val AUTO_COLLECT_WHITE_LIST: String = "auto_collect_white_list"
+const val AUTO_FEED: String = "auto_feed"
